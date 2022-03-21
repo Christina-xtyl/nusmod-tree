d8ac4a97ee969cbab93c7e4a95be9df379cae314
tch1001 <tanchienhao@gmail.com>
2022-03-21 20:06:48 +0800

    selenium

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("hi world")
+from selenium import webdriver
 
-print("tch was here")
+url = 'https://nusmods.com/modules/PL3249/memory'
+
+driver_path = './chromedriver'
+driver = webdriver.Chrome(executable_path=driver_path)
+driver.get(url)
+
+another_url = driver.find_element_by_xpath('//*[@id="prerequisites"]/div/ul/li/ul/li/ul/li[1]/div/a')
+print(another_url)
+
+print("Done")
